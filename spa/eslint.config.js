a2aa24ffcbcc13b4a40336cb381d0054340130b8
Ashley Harwood <ashley.harwood@hotmail.com>
2025-10-03 10:49:48 +1000

    chore: init storybook

--- spa/eslint.config.js
+++ spa/eslint.config.js
@@ -1,3 +1,4 @@
+import storybook from "eslint-plugin-storybook";
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
@@ -5,19 +6,25 @@ import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
+  // JavaScript
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
     plugins: { js },
     extends: ["js/recommended"],
     languageOptions: { globals: globals.browser },
   },
-  tseslint.configs.recommended,
+  // React
   pluginReact.configs.flat.recommended,
+
+  // TypeScript
+  tseslint.configs.recommended,
   {
     rules: {
       "@typescript-eslint/no-explicit-any": "warn",
     },
   },
+
+  // Tailwind
   {
     files: ["tailwind.config.js"],
     languageOptions: {
@@ -28,5 +35,12 @@ export default defineConfig([
     rules: {
       "@typescript-eslint/no-require-imports": "off",
     },
+  },
+
+  // Storybook
+  storybook.configs["flat/recommended"],
+  {
+    files: ["src/**/*.stories.@(ts|tsx|js|jsx|mjs|cjs)"],
+    rules: {},
   },
 ]);
