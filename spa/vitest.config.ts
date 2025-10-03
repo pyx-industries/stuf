068a9a2535c0731d13ce7eb94b99b190a9bacd38
Ashley Harwood <ashley.harwood@hotmail.com>
2025-10-03 15:31:14 +1000

    chore: fix vitest setup

--- spa/vitest.config.ts
+++ spa/vitest.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: "./src/test/setup.ts",
+    setupFiles: "./vitest.setup.ts",
     css: true,
   },
   resolve: {
