10b973b45f28b5504dd88168aaf2b4f25d2dc7ea
Ashley Harwood <ashley.harwood@hotmail.com>
2025-10-13 19:10:55 +1100

    feat: add table agination

--- spa/vitest.setup.ts
+++ spa/vitest.setup.ts
@@ -1,1 +1,21 @@
 import "@testing-library/jest-dom";
+
+// Polyfill for Radix UI pointer capture events
+if (!Element.prototype.hasPointerCapture) {
+  Element.prototype.hasPointerCapture = function () {
+    return false;
+  };
+}
+
+if (!Element.prototype.setPointerCapture) {
+  Element.prototype.setPointerCapture = function () {};
+}
+
+if (!Element.prototype.releasePointerCapture) {
+  Element.prototype.releasePointerCapture = function () {};
+}
+
+// Polyfill for scrollIntoView
+if (!Element.prototype.scrollIntoView) {
+  Element.prototype.scrollIntoView = function () {};
+}
